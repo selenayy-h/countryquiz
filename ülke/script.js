8648583e94200a17275c8f32ad22a0bcc9caddbd
selenayy-h <selenay_hur@icloud.com>
2024-09-30 12:05:19 +0300

    Quiz
    Country

---
+++ ülke/script.js
@@ -0,0 +1,309 @@
+const questions=[
+    {
+        question:"bu bayrak hangi ülkeye aittir",
+        flag: "ekrang/Flag_of_France.svg.png",
+        answers:[
+            {text:"Turkey",correct:false},
+            {text:"France",correct:true},
+            {text:"Germany",correct:false},
+            {text:"Singapur",correct:false},
+        ]
+    },
+
+
+    {
+        question:"bu bayrak hangi ülkeye aittir",
+        flag:"ekrang/Ekran görüntüsü 2024-09-22 211629.png ",
+        answers:[
+            {text:"Arjantin",correct:true},
+            {text:"Arnavutluk",correct:false},
+            {text:"Kazkistan",correct:false},
+            {text:"Yunanistan",correct:false},
+        ]
+    },
+
+    {
+        question:"bu bayrak hangi ülkeye aittir",
+        flag:"ekrang/Ekran görüntüsü 2024-09-22 211403.png",
+        answers:[
+            {text:"Endonezya",correct:true},
+            {text:"El Salvador",correct:false},
+            {text:"Gabon",correct:false},
+            {text:"Güney Kore",correct:false},
+        ]
+    },
+
+    {
+        question:"bu bayrak hangi ülkeye aittir",
+        flag:"ekrang/Ekran görüntüsü 2024-09-22 211651.png",
+        answers:[
+            {text:"İspanya",correct:false},
+            {text:"France",correct:false},
+            {text:"Romanya",correct:true},
+            {text:"Singapur",correct:false},
+        ]
+    },
+    {
+        question:"bu bayrak hangi ülkeye aittir",
+        flag:"ekrang/Ekran görüntüsü 2024-09-22 212125.png",
+        answers:[
+            {text:"Kamerun",correct:false},
+            {text:"İzlanda",correct:true},
+            {text:"Romanya",correct:false},
+            {text:"Japonya",correct:false},
+        ]
+    },
+    {
+        question:"bu bayrak hangi ülkeye aittir",
+        flag:"ekrang/Ekran görüntüsü 2024-09-22 212321.png",
+
+        answers:[
+            {text:"İspanya",correct:false},
+            {text:"Umman",correct:false},
+            {text:"Uruguay",correct:false},
+            {text:"Uganda",correct:true},
+        ]
+    }
+    ,
+    {
+        question:"bu bayrak hangi ülkeye aittir",
+        flag:"ekrang/Ekran görüntüsü 2024-09-22 212440.png",
+        answers:[
+            {text:"Karadağ",correct:false},
+            {text:"İsviçre",correct:false},
+            {text:"Danimarka",correct:true},
+            {text:"Çin",correct:false},
+        ]
+    }
+    ,
+    {
+        question:"bu bayrak hangi ülkeye aittir",
+        flag:"ekrang/Ekran görüntüsü 2024-09-22 212536.png",
+        answers:[
+            {text:"Moldova",correct:false},
+            {text:"Nepal",correct:true},
+            {text:"Myanmar",correct:false},
+            {text:"Nauru",correct:false},
+        ]
+    }
+    ,
+    {
+        question:"bu bayrak hangi ülkeye aittir",
+        flag:"ekrang/Ekran görüntüsü 2024-09-22 212619.png",
+        answers:[
+            {text:"Umman",correct:false},
+            {text:"Yemen",correct:false},
+            {text:"Tunus",correct:true},
+            {text:"Tuvalu",correct:false},
+        ]
+    }
+    ,
+    {
+        question:"bu bayrak hangi ülkeye aittir",
+        flag:"ekrang/Ekran görüntüsü 2024-09-22 212704.png",
+        answers:[
+            {text:"Zambiye",correct:true},
+            {text:"Nijerya",correct:false},
+            {text:"Yemen",correct:false},
+            {text:"Vanuatu",correct:false},
+        ]
+    }
+    ,
+    {
+        question:"bu bayrak hangi ülkeye aittir",
+        flag:"ekrang/Ekran görüntüsü 2024-09-22 212735.png",
+        answers:[
+            {text:"Norveç",correct:true},
+            {text:"isveç",correct:false},
+            {text:"Romanya",correct:false},
+            {text:"Finlandiya",correct:false},
+        ]
+    }
+    
+    ,
+    {
+        question:"bu bayrak hangi ülkeye aittir",
+        flag:"ekrang/Ekran görüntüsü 2024-09-22 212923.png",
+        answers:[
+            {text:"Japonya",correct:false},
+            {text:"Kore",correct:false},
+            {text:"Gürcistan",correct:true},
+            {text:"Peru",correct:false},
+        ]
+    }
+    
+    ,
+    {
+        question:"bu bayrak hangi ülkeye aittir",
+        flag:"ekrang/Ekran görüntüsü 2024-09-22 213031.png",
+        answers:[
+            {text:"Danimarka",correct:false},
+            {text:"Tayland",correct:true},
+            {text:"Nepal",correct:false},
+            {text:"Yunanistan",correct:false},
+        ]
+    }
+    
+    ,
+    {
+        question:"bu bayrak hangi ülkeye aittir",
+         flag:"ekrang/Ekran görüntüsü 2024-09-22 213123.png",
+        answers:[
+            {text:"Vietnam",correct:false},
+            {text:"Portekiz",correct:false},
+            {text:"Şili",correct:false},
+            {text:"Brezilya",correct:true},
+        ]
+    }
+    
+    ,
+    {
+        question:"bu bayrak hangi ülkeye aittir",
+        flag:"ekrang/Ekran görüntüsü 2024-09-22 213254.png",
+        answers:[
+            {text:"Madagaskar",correct:false},
+            {text:"İspanya",correct:true},
+            {text:"Yeni Zelanda",correct:false},
+            {text:"İtalya",correct:false},
+        ]
+    }
+    
+    ,
+    {
+        question:"bu bayrak hangi ülkeye aittir",
+        flag:"ekrang/Ekran görüntüsü 2024-09-22 213353.png",
+        answers:[
+            {text:"Finlandiya",correct:true},
+            {text:"Yunanistan",correct:false},
+            {text:"Norveç",correct:false},
+            {text:"Danimarka",correct:false},
+        ]
+    }
+    
+    ,
+    {
+        question:"bu bayrak hangi ülkeye aittir",
+        flag:"ekrang/Ekran görüntüsü 2024-09-22 213447.png",
+        answers:[
+            {text:"Birleşik Krallik",correct:false},
+            {text:"İzlanda",correct:false},
+            {text:"Avustralya",correct:true},
+            {text:"Fransa",correct:false},
+        ]
+    }
+    ,
+    {
+        question:"bu bayrak hangi ülkeye aittir",
+        flag:"ekrang/Ekran görüntüsü 2024-09-22 213513.png",
+        answers:[
+            {text:"İzlanda",correct:false},
+            {text:"İsviçre",correct:true},
+            {text:"Danimarka",correct:false},
+            {text:"Avusturya",correct:false},
+        ]
+    }
+
+];
+
+const questionElement=document.getElementById("question");
+const answerButtons =document.getElementById("answer-buttons");
+const nextButton=document.getElementById("next-btn");
+
+let currentQuestionIndex=0;
+let score=0;
+
+
+
+function startQuiz(){
+    currentQuestionIndex=0;/*Şu anki sorunun dizideki indeksini (konumunu) tutar. Başlangıç değeri 0'dır, yani ilk soruyla başlar. */
+    score=0;/*kullacnınn doğru cevapladığı sayıları tutar */
+    nextButton.innerHTML="Next";
+/*Başlangıçta buton üzerinde "Başla" yazıyor. Ancak, kullanıcı quiz'e başladığında bu butonun üzerinde "Next" yazmasını istiyorsunuz. İşte bu durumda nextButton
+.innerHTML="Next"; satırı kullanılır. */
+ showQuestion();/** İlk soruyu göstermek için showQuestion() fonksiyonunu çağırır. */
+}
+
+
+function showQuestion() {
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + " ." + currentQuestion.question;
+
+    const flagElement = document.getElementById("flag");
+    flagElement.src = currentQuestion.flag; // Update the flag image for each question
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+}
+
+    function resetState(){
+        nextButton.style.display = 'none'; // "Next" butonunu gizle
+        while (answerButtons.firstChild) {
+            answerButtons.removeChild(answerButtons.firstChild); // Önceki cevapları temizle
+        }
+    }
+    function selectAnswer(e)
+    {
+        const selectedBtn=e.target;
+        const isCorrect=selectedBtn.dataset.correct=="true";//seçilen Datasetin doğru olup olmadığını kontrol edecek
+        if(isCorrect){
+            selectedBtn.classList.add("correct");
+    score++;
+        }
+        else{
+            selectedBtn.classList.add("incorrect");  
+        }
+
+Array.from(answerButtons.children).forEach(button =>{
+    if(button.dataset.correct==="true"){
+        button.classList.add("correct");
+    }
+    button.disabled=true;//her durumda  tüm butonları devre dışı bırakmak için kullanılıyor
+//Bir butonun disabled özelliği true olarak ayarlandığında, kullanıcı bu butona tıklayamaz.
+});
+nextButton.style.display="block";// "Next" butonunu görünür yapar.
+// display: block; elemanı sayfada blok olarak gösterir, yani eleman görünür hale gelir ve tam genişlikte yer kaplar.
+// display: none; elemanı sayfadan gizler, yani eleman görünmez hale gelir ve yer kaplamaz.
+   }
+
+   function showScore(){
+    resetState();
+  
+    questionElement.innerHTML=`You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML="Play Again";
+    nextButton.style.display="block";
+}
+
+   function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex<questions.length){
+        showQuestion();
+    }else{
+        showScore();
+
+    }
+   }
+
+nextButton.addEventListener("click",()=>{
+    if(currentQuestionIndex < questions.length){
+handleNextButton();
+    }else{
+        startQuiz();
+    }
+})
+   
+startQuiz();
+
+
+
+
+
